3aa72e804cdc8f69b9c191a34e18a5a8c736a073
Jossani da Cruz <jossanidacruz2@gmail.com>
2022-10-22 08:45:11 +0100

    playing around with the code

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -113,7 +113,7 @@ function setCardType(type: string) {
     default: ['black', 'gray']
   }
 
-  ccLogoType.setAttribute('src', `./src/assets/cc-${type}.svg`);
+  ccLogoType.setAttribute('src', `/cc-${type}.svg`);
   darkerFillColor.setAttribute('fill', colors[type as keyof typeof colors][0]);
   lighterFillColor.setAttribute('fill', colors[type as keyof typeof colors][1]);
 }
